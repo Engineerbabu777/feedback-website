df2026603e7d0be7051e1ff4acd380af76b46868
Engineerbabu777 <awaismumtaz0099@gmail.com>
2023-08-31 10:14:42 +0500

    f-w-v0.0.4

--- app/component/FeedbackForm.tsx
+++ app/component/FeedbackForm.tsx
@@ -4,6 +4,7 @@ import Popup from './Popup';
 import {useState} from 'react';
 import axios from 'axios';
 import {supabase_Storage} from '../utils/supabase/storage';
+import {AiFillDelete} from 'react-icons/ai';
 
 type Props = {
     close: () => void;
@@ -15,6 +16,9 @@ export default function FeedbackForm({ close }: Props) {
         title: '',
         description:'',
     });
+
+    const [images ,setImages] = useState<any>([]);
+    const [uploading , setUploading] = useState(false);
 
     // FUNCTION TO HANDLE THE STATES!
      const handler = (e:any) => {
@@ -37,15 +41,21 @@ export default function FeedbackForm({ close }: Props) {
       e.preventDefault();
 
       const files = [...e.target.files]; // GETTING FILES PROPERTY FROM EVENT HANDLER !!
+      if(!files.length) return;
+      setUploading(true);
 
-      supabase_Storage(files);
+      const data = await supabase_Storage(files);
       //  const upload = await axios.post('/api/upload',data);
-      //  console.log(upload)
+      setUploading(false);
+       setImages([...images , ...data]);
 
      }
 
-
-
+    // REMOVE ITEM FROM ARRAY!
+    const deleteAttachment = (e:any,val:string) => {
+      e.preventDefault();
+      setImages([...images.filter((v:any,i:any) => v !== val)]);
+    }
 
     return (<>
        
@@ -56,9 +66,19 @@ export default function FeedbackForm({ close }: Props) {
             <input value={suggestion.title} onChange={handler} name="title" className="w-full border rounded-md p-2" type="text" placeholder="A short descriptive , title" />
             <label className="block mt-4 mb-1 text-slate-700">Details</label>
             <textarea value={suggestion.description} onChange={handler} name="description" className="w-full border resize-none rounded-md p-2" placeholder="Please includes any details" />
+           
+           {/* DISPLAYING IMAGES! */}
+           
+            <section className="flex flex-wrap gap-4">
+             {(images?.length>0) && images.map((link:string,ind:number)=>(<div key={ind} className=" relative w-20 h-24 border p-1  rounded-md border-blue-300 border-dashed">
+              <div onClick={(e:any) => deleteAttachment(e,link)} className="absolute -top-1 -right-1 bg-red-300 bg-opacity-50 cursor-pointer text-red-600  h-5 w-5 flex items-center justify-center rounded-full hover:w-7 hover:h-7 transition-all ease-in-out duration-300"><AiFillDelete /></div>
+               {(['jpeg','jfif','jpg','png','gif','webp'].includes(link.split('.')[3])) ? (<><img className="w-full h-full" src={link} alt="text"/></>):(<a href={link} target={"_blank"} className="text-blue-400 ">visit</a>)}
+             </div>))}
+            </section>
+
             <div className="flex gap-2 mt-2 justify-end">
                  <label className="py-2 px-4 text-gray-600 cursor-pointer">
-                   <span >Attach Files</span> 
+                   <span >{uploading ? 'uploading' : 'Attach Files'}</span> 
                    <input multiple onChange={handleAttachFiles} type="file" className="hidden" placeholder="attach files" title="attaching files" />
                  </label>
                 <Button primary onClick={postFeedback} >Create post</Button>
--- app/utils/supabase/storage.ts
+++ app/utils/supabase/storage.ts
@@ -2,24 +2,52 @@
 
 import { createClient } from '@supabase/supabase-js';
 
+async function getViewUrl(bucketName:string, filePath:string|any) {
+    console.log(process.env)
+const supabaseUrl = `https://bovwwnhnvuameioyecfb.supabase.co`;
+const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvdnd3bmhudnVhbWVpb3llY2ZiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MzIzNTE4MSwiZXhwIjoyMDA4ODExMTgxfQ.scOpBrRT1KtKCsAe3CSjkGYQymYFQQQJCg8tcVoyvDw`;
 
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+
+    try {
+        const { data } = supabase
+        .storage
+        .from(bucketName)
+        .getPublicUrl(filePath)
+  
+      return data.publicUrl;
+    } catch (error:any) {
+      console.error('Error:', error.message);
+      return null;
+    }
+  }
 
 export const supabase_Storage = async (array: any) => {
-    const supabaseUrl = `${process.env.SUPABASE_URL}`;
-    const supabaseKey = `${process.env.SUPABASE_KEY}`;
 
-    const supabase = createClient(supabaseUrl, supabaseKey);
+    const supabaseUrl = `https://bovwwnhnvuameioyecfb.supabase.co`;
+const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvdnd3bmhudnVhbWVpb3llY2ZiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MzIzNTE4MSwiZXhwIjoyMDA4ODExMTgxfQ.scOpBrRT1KtKCsAe3CSjkGYQymYFQQQJCg8tcVoyvDw`;
 
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+
+
+let urls = [];
     for (const file of array) {
-        console.log(file.name)
+        
         const { data, error } = await supabase.storage
             .from('feedback-upload') // Replace with your actual bucket name
-            .upload(`/files/${file?.name.split('.')[0]}`, file);
+            .upload(`/files/${file?.name}`, file);
         if (error) {
-            console.log(error)
+            console.log(error.message)
         } else {
-            console.log(data)
+
+            const viewUrl = await getViewUrl('feedback-upload', `/files/${file?.name}`);
+            urls.push(viewUrl);
         }
     }
+
+    return urls;
+
 
 }
